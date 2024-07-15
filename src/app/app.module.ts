bacc61b7ff4e64dec6f3c1b065a1b39f545802a7
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-07-15 15:22:58 -0300

    feat: create componponents home & component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentComponent } from './component/component.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
