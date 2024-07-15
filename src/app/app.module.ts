8f48b2ad03cd24e05043f2275d53ddf38da7fc37
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-07-15 16:24:10 -0300

    feat: add rodape component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ComponentComponent } from './component/component.component';
 import { HomeComponent } from './home/home.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentComponent,
     HomeComponent,
-    CabecalhoComponent
+    CabecalhoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
