b75bc37f2324c828a9749400f3b9b0e9c1969506
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-07-16 18:50:06 -0300

    feat: update pensamento interface & http requests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
 import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
 import { ExcluirPensamentoComponent } from './components/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { EditarPensamentoComponent } from './components/pensamentos/editar-pensamento/editar-pensamento.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'pensamentos', pathMatch: 'full' },
   { path: 'criar-pensamento', component: CriarPensamentoComponent },
   { path: 'pensamentos', component: ListarPensamentoComponent },
   { path: 'pensamentos/excluir/:id', component: ExcluirPensamentoComponent },
+  { path: 'pensamentos/editar/:id', component: EditarPensamentoComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CriarPensamentoComponent } from './components/pensamentos/criar-pensame
 import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
 import { PensamentoComponent } from './components/pensamentos/pensamento/pensamento.component';
 import { ExcluirPensamentoComponent } from './components/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { EditarPensamentoComponent } from './components/pensamentos/editar-pensamento/editar-pensamento.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { ExcluirPensamentoComponent } from './components/pensamentos/excluir-pen
     ListarPensamentoComponent,
     PensamentoComponent,
     ExcluirPensamentoComponent,
+    EditarPensamentoComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
