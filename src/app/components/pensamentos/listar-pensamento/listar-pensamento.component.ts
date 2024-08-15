86e6729224dfb285e04f4c355f7503933f6f13c1
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-08-14 21:56:12 -0300

    feat: alternative refresh component method

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -15,7 +16,10 @@ export class ListarPensamentoComponent implements OnInit {
   favorito: boolean = false;
   listaFavoritos: Pensamento[] = [];
 
-  constructor(private service: PensamentoService) {}
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
     this.service
@@ -35,6 +39,16 @@ export class ListarPensamentoComponent implements OnInit {
           this.haMaisPensamentos = false;
         }
       });
+  }
+
+  // just to follow the course, the function below was not used
+  recarregarComponente() {
+    this.favorito = false;
+    this.paginaAtual = 1;
+
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate([this.router.url])
   }
 
   pesquisarPensamentos(skip: boolean = false) {
